ff725b05b5eecdeddb5d4e7c24bb7637e77276ac
Vadim Taratonov <taratonovv8@gmail.com>
2023-07-24 15:34:34 +0300

    Update enums

--- src/main/java/ru/taratonov/conveyor/enums/EmploymentStatus.java
+++ src/main/java/ru/taratonov/conveyor/enums/EmploymentStatus.java
@@ -1,7 +1,34 @@
 package ru.taratonov.conveyor.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import ru.taratonov.conveyor.exception.IllegalArgumentOfEnumException;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum EmploymentStatus {
-    UNEMPLOYED,
-    SELF_EMPLOYED,
-    BUSINESS_OWNER
+    UNEMPLOYED("unemployed"),
+    SELF_EMPLOYED("self-employed"),
+    BUSINESS_OWNER("business-owner");
+
+    @JsonCreator
+    static EmploymentStatus findValue(String findValue) {
+        return Arrays.stream(EmploymentStatus.values())
+                .filter(value -> value.name().equalsIgnoreCase(findValue))
+                .findFirst()
+                .orElseThrow(() -> IllegalArgumentOfEnumException.createWith(
+                        Arrays.stream(EmploymentStatus.values())
+                        .map(EmploymentStatus::getTitle)
+                        .collect(Collectors.toList())));
+    }
+
+    private String title;
+
+    EmploymentStatus(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
--- src/main/java/ru/taratonov/conveyor/enums/MaritalStatus.java
+++ src/main/java/ru/taratonov/conveyor/enums/MaritalStatus.java
@@ -1,6 +1,33 @@
 package ru.taratonov.conveyor.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import ru.taratonov.conveyor.exception.IllegalArgumentOfEnumException;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum MaritalStatus {
-    DIVORCED,
-    MARRIED
+    DIVORCED("divorced"),
+    MARRIED("married");
+
+    @JsonCreator
+    static MaritalStatus findValue(String findValue) {
+        return Arrays.stream(MaritalStatus.values())
+                .filter(value -> value.name().equalsIgnoreCase(findValue))
+                .findFirst()
+                .orElseThrow(() -> IllegalArgumentOfEnumException.createWith(
+                        Arrays.stream(MaritalStatus.values())
+                        .map(MaritalStatus::getTitle)
+                        .collect(Collectors.toList())));
+    }
+
+    private String title;
+
+    MaritalStatus(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
