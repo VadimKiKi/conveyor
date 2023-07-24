69e11be0c1886df36befccc2adcd1fd70eae67d8
Vadim Taratonov <taratonovv8@gmail.com>
2023-07-24 18:08:44 +0300

    Add swagger configuration

--- src/main/java/ru/taratonov/conveyor/ConveyorApplication.java
+++ src/main/java/ru/taratonov/conveyor/ConveyorApplication.java
@@ -1,11 +1,25 @@
 package ru.taratonov.conveyor;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Conveyor REST Api",
+                description = "1 MVP level conveyor",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Taratonov Vadim",
+                        email = "taratonovv8@bk.ru",
+                        url = "https://github.com/VadimKiKi/conveyor"
+                )
+        )
+)
 public class ConveyorApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConveyorApplication.class, args);
     }
--- src/main/java/ru/taratonov/conveyor/controller/ConveyorController.java
+++ src/main/java/ru/taratonov/conveyor/controller/ConveyorController.java
@@ -1,5 +1,10 @@
 package ru.taratonov.conveyor.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.taratonov.conveyor.dto.CreditDTO;
+import ru.taratonov.conveyor.dto.ErrorDTO;
 import ru.taratonov.conveyor.dto.LoanApplicationRequestDTO;
 import ru.taratonov.conveyor.dto.LoanOfferDTO;
 import ru.taratonov.conveyor.dto.ScoringDataDTO;
@@ -18,6 +24,7 @@ import java.util.List;
 @RestController
 
 @RequestMapping("/conveyor")
+@Tag(name = "Conveyor Controller", description = "Managing loan offers")
 public class ConveyorController {
 
     private final ConveyorService conveyorService;
@@ -28,12 +35,42 @@ public class ConveyorController {
     }
 
     @PostMapping("/offers")
-    public List<LoanOfferDTO> getPossibleLoanOffers(@RequestBody @Valid LoanApplicationRequestDTO loanApplicationRequest) {
+    @Operation(summary = "Get loan offers", description = "Allows to get 4 loan offers for person")
+    @ApiResponse(
+            responseCode = "200",
+            description = "List of offers received!",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = LoanOfferDTO.class)))
+    @ApiResponse(
+            responseCode = "400",
+            description = "Prescoring failed",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class)))
+    public List<LoanOfferDTO> getPossibleLoanOffers(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Loan request",
+            content = @Content(schema = @Schema(implementation = LoanApplicationRequestDTO.class)))
+                                                    @RequestBody
+                                                    @Valid LoanApplicationRequestDTO loanApplicationRequest) {
         return conveyorService.getOffers(loanApplicationRequest);
     }
 
     @PostMapping("/calculation")
-    public CreditDTO calculateLoanParameters(@RequestBody @Valid ScoringDataDTO scoringData) {
+    @Operation(summary = "Get loan parameters", description = "Allows to get all parameters for credit")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Parameters received!",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CreditDTO.class)))
+    @ApiResponse(
+            responseCode = "400",
+            description = "Scoring failed",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ErrorDTO.class)))
+    public CreditDTO calculateLoanParameters(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "All information about user for calculation loan parameters",
+            content = @Content(schema = @Schema(implementation = ScoringDataDTO.class)))
+                                             @RequestBody
+                                             @Valid ScoringDataDTO scoringData) {
         return conveyorService.calculateParameters(scoringData);
     }
 }
